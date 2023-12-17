63fb11dc14ea46b85fd22c2e22b0e1ca05cf6d9f
Deepak Kumar <deepaklg02@gmail.com>
2023-12-17 21:32:20 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import PIC from "./Assets/pic.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 id = "iqlipse">
+          &lt;<span id="logo">iQlipse</span>/&gt;
+        </h1>
+        <div className="items">
+          <p class="item">&lt;Start/&gt;</p>
+          <p class="item">&lt;Work/&gt;</p>
+          <p class="item">&lt;About/&gt;</p>
+          <p class="item">&lt;Contact/&gt;</p>
+        </div>
       </header>
+      <div id="info">
+        <h1>
+          Hi,My name is <span id="name">Deepak Kumar</span>
+        </h1>
+        <h1>
+          I <span id="design">design</span> and{" "}
+          <span id="develop">develop</span> things
+        </h1>
+      </div>
+      <div id="display">
+        <img src={PIC} alt="iQlipse's Pic" />
+      </div>
     </div>
   );
 }
---
+++ src/components/Tick.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Tick = () => {
+  return <div>Tick</div>;
+};
