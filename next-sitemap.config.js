acd54d989cc5a709e859b1ba2937a19b937862e6
Deepak Kumar <deepaklg02@gmail.com>
2025-03-07 16:49:26 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://iqlipse.tech", // Replace with your actual domain
+  generateRobotsTxt: true, // Generates robots.txt
+};
